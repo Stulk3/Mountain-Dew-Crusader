62bb9b1bcf6d91cae80ad645308f3c88cf68164f
Stulk3 <stulk7746@gmail.com>
2021-12-25 02:03:22 +0300

    Полное удаление Bolt

--- Assets/Scripts/Jotaro/MovementScript.cs
+++ Assets/Scripts/Jotaro/MovementScript.cs
@@ -4,16 +4,52 @@ using UnityEngine;
 
 public class MovementScript : MonoBehaviour
 {
-    Rigidbody2D JotaroRigidBody;
+    [SerializeField] float MovementMultiplier = 3;
+    Transform JotaroTransform;
+    Animator JotaroAnimator;
+    SpriteRenderer JotaroSprite;
+    float Movement;
     
     void Start()
     {
-        JotaroRigidBody = this.GetComponent<Rigidbody2D>();
+        JotaroTransform = this.GetComponent<Transform>();
+        JotaroAnimator = this.GetComponent<Animator>();
+        JotaroSprite = this.GetComponent<SpriteRenderer>();
     }
 
     
     void Update()
     {
-        
+
+        Movement = Input.GetAxis("Horizontal");
+        if (Movement != 0) 
+        {
+            JotaroTransform.Translate( new Vector2(Movement, 0) * MovementMultiplier * Time.deltaTime);
+            RunAnimation();
+        }
+        else
+        {
+            IdleAnimation();
+        }
+        if (Movement < 0) 
+        { 
+            JotaroSprite.flipX = true;
+        }
+        if (Movement > 0)
+        {
+            JotaroSprite.flipX = false;
+        }
+        Debug.Log(Movement);    
+    }
+
+
+    void RunAnimation()
+    {
+        JotaroAnimator.Play("Run");
+    }
+    
+    void IdleAnimation()
+    {
+        JotaroAnimator.Play("Idle");
     }
 }
