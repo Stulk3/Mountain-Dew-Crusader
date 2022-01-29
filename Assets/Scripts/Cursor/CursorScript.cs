d972819bd59282a309fb1ab6667b2b45a4d8821b
Big Boss <stulk7746@gmail.com>
2022-01-29 17:47:14 +0300

    Main Menu Change

--- Assets/Scripts/Cursor/CursorScript.cs
+++ Assets/Scripts/Cursor/CursorScript.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+[RequireComponent(typeof(SpriteRenderer))]
 public class CursorScript : MonoBehaviour
 {
 
-    public LayerMask ClickableILayer;
+    public LayerMask ClickableLayer;
     private Ray Ray;
     [SerializeField] Sprite DefaultCursor;
     [SerializeField] Sprite OnClicktCursor;
@@ -15,15 +15,16 @@ public class CursorScript : MonoBehaviour
 
     void Awake()
     {
-        Cursor.visible = false;
+        
         CursorRenderer = this.GetComponent<SpriteRenderer>();
         CursorRenderer.sprite = DefaultCursor;
     }
 
     void Update()
     {
+        Cursor.visible = false;
         CursorWorldCoordinatesSet();
-        Debug.DrawRay(this.transform.position, Ray.direction, Color.green, 2);
+        Debug.DrawRay(this.transform.position, Ray.direction, Color.green);
         RayCast();
     }
 
@@ -31,15 +32,13 @@ public class CursorScript : MonoBehaviour
     void RayCast()
     {
         Ray = Camera.main.ScreenPointToRay(Input.mousePosition) ;
-        if (Physics.Raycast(Ray,1000, ClickableILayer))
+        if (Physics.Raycast(Ray, ClickableLayer))
         {
             CursorRenderer.sprite = OnClicktCursor;
-            Debug.Log("Да");
         }
         else
         {
             CursorRenderer.sprite = DefaultCursor;
-            Debug.Log("Нет");
         }
     }
 
